a9c4431e6cf43a981b10d45a1365c3c3c7b48a8b
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-02-04 18:43:18 +0000

    Add landing page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import Nav from '../components/nav'
-
 export default () => (
-  <div>
-    <Nav />
-    <main className="p-4">
-      <div className="bg-yellow-300 p-4 rounded-lg">Scan an Egg</div>
+  <div className="min-h-screen bg-red-400 text-white">
+    <main className="container mx-auto p-4 py-12 text-center">
+      <h1 className="font-black text-5xl mb-8">THE GREAT EGG TRAIL</h1>
+      <p className="mb-8">
+        Follow the trail and discover digital eggs on the way. There are a
+        possible 46 eggs to find into total. Best of luck!
+      </p>
+      <a className="block bg-white text-gray-800 py-2 rounded-full border-2 border-gray-800 text-lg font-bold shadow-lg">
+        Get Started
+      </a>
     </main>
   </div>
 )
