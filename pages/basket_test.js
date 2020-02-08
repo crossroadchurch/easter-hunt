42c90fa44608010376f4a225e8cf1690d4b7e069
nikukatansa <nikukatansa@users.noreply.github.com>
2020-02-08 11:14:52 +0000

    Added basket and egg locations

--- pages/basket_test.js
+++ pages/basket_test.js
@@ -1,10 +1,24 @@
 import Basket from '../components/basket'
+import { useState } from 'react'
 
 export default () => {
-  const found = [{egg_id: "02"}, {egg_id: "05"}, {egg_id: "26"}, {egg_id: "39"}]
+  const [count, setCount] = useState(0)
+  const [found, setFound] = useState([])
+
+  function add_egg() {
+    setCount(prev_count => (prev_count + 1) % 43)
+    if (count != 0){
+      setFound([...found, {egg_id: (count-1).toString().padStart(2, '0')}])
+    } else {
+      setFound([])
+    }
+  }
 
   return (
     <main className="container">
+      <button onClick={add_egg} className="bg-green-200 text-green-900 px-4 py-2 text-sm font-medium rounded-full whitespace-no-wrap ml-2">
+        Add an egg
+      </button>
       <Basket found_eggs={found} />
     </main>
   )
