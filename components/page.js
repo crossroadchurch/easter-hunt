4af27085e87353da2a0cff4e0da689c2ef94c2bc
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-02-20 18:19:15 +0000

    Add icons and redirect from play if team is undefined

--- components/page.js
+++ components/page.js
@@ -7,6 +7,11 @@ export default ({ children, title = 'Great Egg Trail', description = '' }) => (
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" type="image/png" href="/images/icons/icon-128x128.png" />
+      <link
+        rel="apple-touch-icon"
+        href="/images/icons/apple-touch-icon.png"
+      ></link>
       <title>{title + ' | Great Egg Trail'}</title>
     </Head>
     <div>{children}</div>
--- pages/play.js
+++ pages/play.js
@@ -2,10 +2,24 @@ import Header from '../components/play/header'
 import Trail from '../components/play/trail'
 import { eggs } from '../qr_codes/easter-data.json'
 import Page from '../components/page'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useLocalStorage } from 'react-use'
 
-export default () => (
-  <Page title="Play">
-    <Header />
-    <Trail eggs={eggs} />
-  </Page>
-)
+export default () => {
+  const router = useRouter()
+  const [team] = useLocalStorage('team')
+
+  useEffect(() => {
+    if (!team) {
+      router.push('/welcome')
+    }
+  }, [router])
+
+  return (
+    <Page title="Play">
+      <Header />
+      <Trail eggs={eggs} />
+    </Page>
+  )
+}
